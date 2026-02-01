d1a7cc2e716b5dc6efa68cbb95a288ad079fb7f0
PBK <cody.bk.peng@gmail.com>
2026-02-01 12:37:38 +0800

    Add wiring

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define MCU_VERSION "v1.0"
+#define MCU_BUILD_DATE "2024 / 06 / 01"
+
 #include <Arduino.h>
 
 // =================   Communication Protocol & Related Variables   ================
@@ -25,7 +28,7 @@
  *    5     pump_overheat_protect 氣泵過熱溫度保護
  */
 
- // MCU => HMI
+// MCU => HMI
 struct out
 {
 	String message = "一切正常";
@@ -66,12 +69,60 @@ struct out
 		int valve_auto_release_time = 30;	// 自動釋放時長		; in seconds
 		int pump_overheat_protect = 30;		// 氣泵過熱溫度保護	; in degree Celsius
 
-		const String hmi_version = "2026 / 01 / 30 | v1";
-		const String mcu_version = "2026 / 01 / 30 | v1";
+		// const String hmi_version = "2026 / 01 / 30 | v1.0";
+		const String mcu_version = String(MCU_BUILD_DATE) + " | " + String(MCU_VERSION);
 	};
 };
 
 // =================================================================================
+
+// =================   Wiring   ================
+constexpr int PIN_PumpRelay1 = PB12;
+constexpr int PIN_PumpRelay2 = PB13;
+constexpr int PIN_PumpRelay3 = PB14;
+constexpr int PIN_PumpRelay4 = PB15;
+constexpr int PIN_PumpRelay5 = PA11;
+constexpr int PIN_PumpRelay6 = PA12;
+
+constexpr int PIN_Servo1 = PA8;
+constexpr int PIN_Servo2 = PA9;
+constexpr int PIN_FanPWM = PA10;
+
+constexpr int PIN_MUX_S0 = PA15;
+constexpr int PIN_MUX_S1 = PB3;
+constexpr int PIN_MUX_S2 = PB4;
+constexpr int PIN_MUX_S3 = PB5;
+constexpr int PIN_MUX_SIG = PA1; // ADC
+/*
+ *	MUX Channel Mapping:
+ *	Channel  Signal
+ *	=======  ======
+ *	 0 ~ 5	 Pump Temperature Sensors 1 to 6
+ *	 6		 Ambient Light Sensor
+ *	 7 ~ 12  CNC Input Signal Level (Rotary Switch)
+ *	 13      CNC Input (Optalcoupler)
+ *   14      L Input Signal Level (Potentiometer)
+ *   15      R Input Signal Level (Potentiometer)
+ */
+
+constexpr int PIN_WLED = PB10;
+
+/*
+ *	Other Pins Used:
+ *	Pin		Function
+ *	===		========
+ *	PC13	Onboard Status LED
+ *  PA2	    Serial TX (to HMI)
+ *  PA3	    Serial RX (from HMI)
+ *  PA4	    nRF24L01 CSN
+ *  PA5	    nRF24L01 SCK
+ *  PB6     nRF24L01 MISO
+ *  PB7     nRF24L01 MOSI
+ *  R       Reset Button
+ *  ST-link ST-link Programmer
+ *  BOOT0   Boot0 Switch
+ */
+// ===========================================
 
 void setup() {
 	Serial.begin(115200);
